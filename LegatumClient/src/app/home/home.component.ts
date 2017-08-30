0acec31c37a505e8ac5948e72fc3627b3a2d36b8
Mike Doyle <mikedoyle007@gmail.com>
2017-08-30 13:05:31 -0700

    Add handleLogin function that uses sweetalert2

--- LegatumClient/src/app/home/home.component.ts
+++ LegatumClient/src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var swal: any;
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -12,4 +14,13 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
+  handleLogin(): void {
+    console.log('clicked');
+    swal({
+      title: 'Login',
+      html:
+      '<input type="email" placeholder="email" id="swal-input1" class="swal2-input">' +
+      '<input type="password" placeholder="password" id="swal-input2" class="swal2-input">',
+    });
+  }
 }
