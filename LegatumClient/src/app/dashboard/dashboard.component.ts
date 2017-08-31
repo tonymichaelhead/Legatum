997050fc1cfe615a23b08d4dd14b5458c2bccd8b
James Knippel <jamesknippel@Jamess-MacBook-Pro.local>
2017-08-31 06:00:51 -0700

    smart conttracts scaffold

--- LegatumClient/src/app/dashboard/dashboard.component.ts
+++ LegatumClient/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+
+import * as Web3 from 'web3';
+import * as contract from 'truffle-contract';
+import * as will from '../../../build/contracts/will.json';
+
+console.log(will);
+declare var window: any;
+
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -7,9 +16,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  web3: any;
+
   constructor() { }
 
+  checkAndInstantiateWeb3 = () => {
+    // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+    if (typeof typeof window.web3 !== 'undefined') {
+      console.log(
+        "Using web3 detected from external source. If you find that your accounts don\'t appear or you have 0 MetaCoin, ensure you\'ve configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask"
+      );
+      // Use Mist/MetaMask's provider
+      this.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      console.log(
+        "No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it\'s inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask"
+      );
+      // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+      this.web3 = new Web3(
+        new Web3.providers.HttpProvider('http://localhost:8545')
+      );
+    }
+  }
+
+
+
   ngOnInit() {
+    this.checkAndInstantiateWeb3();
   }
 
 }
