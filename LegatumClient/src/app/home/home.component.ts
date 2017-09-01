b05f5b2ba72b5a8ebd40cf64b19ba8cbbe49f727
Mike Doyle <mikedoyle007@gmail.com>
2017-09-01 11:41:33 -0700

    Merge conflict resolution

--- LegatumClient/src/app/home/home.component.ts
+++ LegatumClient/src/app/home/home.component.ts
@@ -49,24 +49,38 @@ export class HomeComponent implements OnInit {
       }
     }).then(function (result) {
       // authenticate user with stored data passed to the result variable
-      // email = result[0]
-      // password = result[1]
-      console.log('result = ', result);
-      
+      const userEmail = result[0];
+      const userPassword = result[1];
+      const resultObj = context.afAuth.auth.signInWithEmailAndPassword(userEmail, userPassword)
+        .then(data => console.log(data, 'success for reals'))
+        .catch(error => console.log(error, 'error occurred'));
+      console.log('resultObj = ', resultObj);
+      console.log('success!!!');
     }, function (dismiss) {
+      // 'cancel' in this case refers to the register button
       if (dismiss === 'cancel') {
         swal({
           title: 'Register',
           confirmButtonText: 'Register',
           showCancelButton: true,
           html:
-          '<input type="email" placeholder="email" class="swal2-input">' +
-          '<input type="password" placeholder="password" class="swal2-input">' +
-          '<input type="text" placeholder="ssn" class="swal2-input">',
-        }).then(function (text) {
-          if (text) {
-            swal(text);
+            '<input id="register-email" type="email" placeholder="email" class="swal2-input">' +
+            '<input id="register-password" type="password" placeholder="password" class="swal2-input">',
+          preConfirm: function () {
+            return new Promise(function (resolve) {
+              resolve([
+                $('#register-email').val(),
+                $('#register-password').val()
+              ]);
+            });
           }
+        }).then(function (result2) {
+          console.log('result2 = ', result2);
+          const userEmail2 = result2[0];
+          const userPassword2 = result2[1];
+          const resultObj2 = context.afAuth.auth.createUserWithEmailAndPassword(userEmail2, userPassword2)
+            .then(success => console.log(success, 'Success registering user'))
+            .catch(error => console.log(error, 'error occurred'));
         });
       }
     });
