9f1ea0be88cb98520a3dc6852418792351c4e155
Mike Doyle <mikedoyle007@gmail.com>
2017-09-04 18:02:36 -0700

    Put register function inside its own component

--- LegatumClient/src/app/register/register.component.ts
+++ LegatumClient/src/app/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+
+declare var swal: any;
+declare var $: any;
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) { }
 
   ngOnInit() {
+  }
+
+  handleRegister(): void {
+    const context = this;
+    swal({
+      title: 'REGISTER',
+      confirmButtonText: 'REGISTER',
+      confirmButtonColor: '#830083',
+      confirmButtonClass: 'register-btn',
+      reverseButtons: true,
+      showCancelButton: true,
+      cancelButtonColor: 'transparent',
+      cancelButtonText: 'CANCEL',
+      cancelButtonClass: 'cancel-btn',
+      customClass: 'register-popup',
+      background: '',
+      html:
+      '<input id="register-email" type="email" placeholder="email" class="swal2-input">' +
+      '<input id="register-password" type="password" placeholder="password" class="swal2-input">',
+      preConfirm: function () {
+        return new Promise(function (resolve) {
+          resolve([
+            $('#register-email').val(),
+            $('#register-password').val()
+          ]);
+        });
+      },
+      onOpen: function () {
+        $('#register-email').focus();
+      }
+    }).then(function (result2) {
+      const userEmail2 = result2[0];
+      const userPassword2 = result2[1];
+      const resultObj2 = context.afAuth.auth.createUserWithEmailAndPassword(userEmail2, userPassword2)
+        .then(success => {
+          console.log(success, 'Success registering user. Now we need to display success message and redirect to login');
+        })
+        .catch(error => {
+          console.log(error, 'error occurred. Now we need to display error message.');
+        });
+    });
   }
 
 }
