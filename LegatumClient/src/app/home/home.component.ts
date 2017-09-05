553d5a57eba4005923beb812126be029d085644f
Mike Doyle <mikedoyle007@gmail.com>
2017-09-05 10:04:24 -0700

    Update signup register function to use the register component

--- LegatumClient/src/app/home/home.component.ts
+++ LegatumClient/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginComponent } from '../login/login.component';
-import { AuthService } from '../auth.service'; 
+import { AuthService } from '../auth.service';
+import { RegisterComponent } from '../register/register.component';
 
 @Component({
   selector: 'app-home',
@@ -13,13 +14,14 @@ export class HomeComponent implements OnInit {
 
   constructor(
     private authService: AuthService,
-    private login: LoginComponent) {}
+    private login: LoginComponent,
+    private register: RegisterComponent) {}
   
   //Check to see if user is logged in and if so display logged in features
   checkIfLoggedIn() {
     if (this.authService.isLoggedIn) {
       console.log('Is logged in: true');
-        this.isLoggedIn = true; 
+        this.isLoggedIn = true;
     } else {
       console.log('User is not logged in');
     }
@@ -34,7 +36,7 @@ export class HomeComponent implements OnInit {
   }
 
   handleRegister(): any {
-    this.login.handleRegister();
+    this.register.registerUser();
   }
 
 }
