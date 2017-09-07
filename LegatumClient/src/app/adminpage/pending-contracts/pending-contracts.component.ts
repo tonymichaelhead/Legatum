475d7aeba1441e02976d7e650014d46795942522
James Knippel <jamesknippel@Jamess-MacBook-Pro.local>
2017-09-07 13:10:16 -0700

    admin panel effectively scaffolded, routed and ready

--- LegatumClient/src/app/adminpage/pending-contracts/pending-contracts.component.ts
+++ LegatumClient/src/app/adminpage/pending-contracts/pending-contracts.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pending-contracts',
@@ -6,8 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pending-contracts.component.css']
 })
 export class PendingContractsComponent implements OnInit {
+  contracts = [
+    {
+      contractNickname: 'Another will',
+      contractId: '123489-0',
+      createdAt: '01/01/0001'
+    },
+    {
+      contractNickname: 'cool will',
+      contractId: '000000',
+      createdAt: '12/25/2000'
+    },
+    {
+      contractNickname: 'Smart Contract',
+      contractId: '81818181818',
+      createdAt: '04/01/3001'
+    }
+  ];
 
-  constructor() { }
+  constructor(private router: Router ) { }
+
+  @Output() onCreateFormClick = new EventEmitter();
 
   ngOnInit() {
   }
