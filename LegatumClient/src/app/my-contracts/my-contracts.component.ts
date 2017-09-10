b7b791f4e434ad8585842a4e47a8e8569b0d6ee2
James Knippel <jamesknippel@gmail.com>
2017-09-09 18:12:57 -0700

    pending contracts render

--- LegatumClient/src/app/my-contracts/my-contracts.component.ts
+++ LegatumClient/src/app/my-contracts/my-contracts.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router'
+import { Router, ActivatedRoute } from '@angular/router';
 import { DashboardService } from '../dashboard.service';
 import { AuthService } from '../auth.service';
 import { HttpClient, HttpParams } from '@angular/common/http';
 
-import {Observable} from 'rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/share';
-import {Observer} from 'rxjs/Observer';
+import { Observer } from 'rxjs/Observer';
 
 @Component({
   selector: 'app-my-contracts',
@@ -45,12 +45,12 @@ export class MyContractsComponent implements OnInit {
   getUserContracts() {
     this.subscription = this.dashboardService.contractsChange$.subscribe( item => {
       this.contracts = item;
-    })
+    });
     console.log('The reset contracts in my-contracts: ', this.contracts);
   }
 
   ngOnInit() {
-    //Get user contracts
+    // Get user contracts
     this.dashboardService.getAndSetContracts();
     this.contracts = this.dashboardService.currentContracts();
     this.getUserContracts();
