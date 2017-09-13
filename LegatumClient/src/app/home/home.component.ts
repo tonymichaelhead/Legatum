bea926e2881598947b6d275637bb2eb1b864fb56
James Knippel <jamesknippel@gmail.com>
2017-09-12 20:53:17 -0700

    css behavior improvements

--- LegatumClient/src/app/home/home.component.ts
+++ LegatumClient/src/app/home/home.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit, Output } from '@angular/core';
+import { Component, OnInit, OnDestroy, Output } from '@angular/core';
 import { LoginComponent } from '../login/login.component';
 import { AuthService } from '../auth.service';
 import { RegisterComponent } from '../register/register.component';
 import { Router, ActivatedRoute } from '@angular/router';
+import { MnFullpageModule, MnFullpageService } from 'ngx-fullpage';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
   isLoggedIn: boolean = false;
   urlParam: string;
@@ -21,7 +22,9 @@ export class HomeComponent implements OnInit {
   constructor(
     private authService: AuthService,
     private login: LoginComponent,
-    private register: RegisterComponent) { }
+    private register: RegisterComponent,
+    private fullPageService: MnFullpageService) {
+     }
 
   // Check to see if user is logged in and if so display logged in features
   checkIfLoggedIn() {
@@ -42,128 +45,140 @@ export class HomeComponent implements OnInit {
     this.register.registerUser();
   }
 
-  ngOnInit() {
-    this.checkIfLoggedIn();
-    this.myStyle = {
-      'position': 'fixed',
-      'width': '100%',
-      'height': '100%',
-      'z-index': -1,
-      'top': 0,
-      'left': 0,
-      'right': 0,
-      'bottom': 0,
-    };
-    this.myParams = {
-      particles: {
-        number: {
-          'value': 75,
-          'density': {
-            'enable': true,
-            'value_area': 500
-          }
-        },
-        color: {
-          'value': '#743ade'
-        },
-        shape: {
-          'type': 'triangle',
-          'stroke': {
-            'width': 2,
-            'color': '#ffb3ff'
-          },
-          'polygon': {
-            'nb_sides': 4
-          },
-          'image': {
-            'src': './eth.svg',
-            'width': 100,
-            'height': 100
-          }
-        },
-        opacity: {
-          'value': 0.5,
-          'random': false,
-          'anim': {
-            'enable': false,
-            'speed': 1,
-            'opacity_min': 0.2,
-            'sync': false
-          }
-        },
-        size: {
-          'value': 4,
-          'random': true,
-          'anim': {
-            'enable': true,
-            'speed': 40,
-            'size_min': 0.1,
-            'sync': false
-          }
-        },
-        line_linked: {
-          'enable': true,
-          'distance': 150,
-          'color': '#932ab3',
-          'opacity': 0.9,
-          'width': 1
-        },
-        move: {
-          'enable': true,
-          'speed': 2,
-          'direction': 'bottom-right',
-          'random': true,
-          'straight': true,
-          'out_mode': 'out',
-          'bounce': true,
-          'attract': {
-            'enable': false,
-            'rotateX': 600,
-            'rotateY': 1200
-          }
-        }
-      },
-      interactivity: {
-        'detect_on': 'canvas',
-        'events': {
-          'onhover': {
-            'enable': true,
-            'mode': 'repulse'
-          },
-          onclick: {
-            'enable': true,
-            'mode': 'repulse'
-          },
-          'resize': true
-        },
-        modes: {
-          'grab': {
-            'distance': 150,
-            'line_linked': {
-              'opacity': 1
-            }
-          },
-          bubble: {
-            'distance': 400,
-            'size': 40,
-            'duration': 2,
-            'opacity': 1,
-            'speed': 3
-          },
-          repulse: {
-            'distance': 200
-          },
-          push: {
-            'particles_nb': 4
-          },
-          remove: {
-            'particles_nb': 2
-          }
-        }
-      },
-      retina_detect: true,
-    };
+  ngOnDestroy () {
+    this.fullPageService.destroy('all');
+    this.myParams['particles']['move']['enable'] = false;
   }
 
+  ngOnInit() {
+    this.checkIfLoggedIn();
+    this.initParticles();
+  }
+
+  initParticles() {
+    if (this.myParams['particles']) {
+      return;
+    } else {
+      this.myStyle = {
+        'position': 'fixed',
+        'width': '100%',
+        'height': '100%',
+        'z-index': -1,
+        'top': 0,
+        'left': 0,
+        'right': 0,
+        'bottom': 0,
+      };
+      this.myParams = {
+        particles: {
+          number: {
+            'value': 50,
+            'density': {
+              'enable': true,
+              'value_area': 500
+            }
+          },
+          color: {
+            'value': '#743ade'
+          },
+          shape: {
+            'type': 'triangle',
+            'stroke': {
+              'width': 2,
+              'color': '#ffb3ff'
+            },
+            'polygon': {
+              'nb_sides': 4
+            },
+            'image': {
+              'src': './eth.svg',
+              'width': 100,
+              'height': 100
+            }
+          },
+          opacity: {
+            'value': 0.5,
+            'random': false,
+            'anim': {
+              'enable': false,
+              'speed': 1,
+              'opacity_min': 0.2,
+              'sync': false
+            }
+          },
+          size: {
+            'value': 4,
+            'random': true,
+            'anim': {
+              'enable': true,
+              'speed': 40,
+              'size_min': 0.1,
+              'sync': false
+            }
+          },
+          line_linked: {
+            'enable': true,
+            'distance': 150,
+            'color': '#932ab3',
+            'opacity': 0.9,
+            'width': 1
+          },
+          move: {
+            'enable': true,
+            'speed': 2,
+            'direction': 'bottom-right',
+            'random': true,
+            'straight': true,
+            'out_mode': 'out',
+            'bounce': true,
+            'attract': {
+              'enable': false,
+              'rotateX': 600,
+              'rotateY': 1200
+            }
+          }
+        },
+        interactivity: {
+          'detect_on': 'canvas',
+          'events': {
+            'onhover': {
+              'enable': true,
+              'mode': 'repulse'
+            },
+            onclick: {
+              'enable': true,
+              'mode': 'repulse'
+            },
+            'resize': true
+          },
+          modes: {
+            'grab': {
+              'distance': 150,
+              'line_linked': {
+                'opacity': 1
+              }
+            },
+            bubble: {
+              'distance': 400,
+              'size': 40,
+              'duration': 2,
+              'opacity': 1,
+              'speed': 3
+            },
+            repulse: {
+              'distance': 200
+            },
+            push: {
+              'particles_nb': 4
+            },
+            remove: {
+              'particles_nb': 2
+            }
+          }
+        },
+        retina_detect: true,
+      };
+    }
+  }
 
 }
