0f220b3009eb36309ae50cfdad6bfdfccf1aabfc
Aren Rostamian <arenrostamian@gmail.com>
2017-09-13 22:28:57 -0700

    Styling goodies for contracts page

--- LegatumClient/src/app/contract-form/contract-form.component.ts
+++ LegatumClient/src/app/contract-form/contract-form.component.ts
@@ -23,7 +23,7 @@ export class ContractFormComponent {
 
   newContract: any = {
     contract_nickname: '',
-    will_text: 'No text',
+    will_text: '',
     file_name: 'No file attached.',
     beneficiary: '',
     hash: 'rstrastaarst'
