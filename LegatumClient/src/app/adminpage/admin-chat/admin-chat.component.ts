29eedbdc91e834adb1117b7cd357828cdd734a23
Mike Doyle <mikedoyle007@gmail.com>
2017-09-14 15:34:50 -0700

    Style admin-chat-component

---
+++ LegatumClient/src/app/adminpage/admin-chat/admin-chat.component.ts
@@ -0,0 +1,126 @@
+import { Component, OnInit } from '@angular/core';
+import { SocketIoModule, SocketIoConfig, Socket } from 'ng-socket-io';
+
+declare var $: any;
+
+@Component({
+  selector: 'app-admin-chat',
+  templateUrl: './admin-chat.component.html',
+  styleUrls: ['./admin-chat.component.css']
+})
+export class AdminChatComponent implements OnInit {
+
+  chatInitiated: boolean;
+  haveUsername: boolean;
+  roomAvailable: boolean;
+  chatEnded: boolean;
+  socket = io();
+  username: string;
+  userID: string;
+  adminName: string;
+  adminID: string;
+  queueSize: number;
+
+  constructor() {
+
+    this.chatInitiated = false;
+    this.haveUsername = false;
+    this.roomAvailable = false;
+    this.chatEnded = false;
+    this.username = '';
+    this.userID = '';
+    this.adminName = '';
+    this.adminID = '';
+    this.queueSize = 0;
+
+  }
+
+  // init chat
+  getQueueSize(): void {
+    this.socket.emit('getQueueSize');
+  }
+
+  getUserCredentials(): void {
+    this.socket.emit('getUserSocketid');
+    this.socket.emit('getUsername');
+  }
+
+  handleChatRequest(): void {
+    this.chatInitiated = true;
+    this.haveUsername = false;
+    if (!this.socket.connected) {
+      this.socket.connect();
+    }
+  }
+
+  handleAdminAccept(): void {
+    this.haveUsername = true;
+    this.adminName = $('#chat-username').val() || 'admin';
+    this.adminID = this.socket.id;
+    console.log('admin id is ', this.adminID);
+    this.socket.emit('adminAcceptChat', this.adminName, this.adminID);
+    this.roomAvailable = true;
+  }
+
+  // handle chat
+  handleSendMessage(): void {
+    const message = $('#chat-input').val();
+    this.socket.emit('chatMessage', message, this.adminName);
+    $('#chat-input').val('');
+    console.log('ADMIN sending message of', message);
+  }
+
+  // end chat
+  handleEndChat(): void {
+    this.getUserCredentials();
+    this.chatInitiated = false;
+    this.roomAvailable = false;
+    this.username = '';
+    this.userID = '';
+    this.chatEnded = true;
+    this.haveUsername = false;
+    this.socket.emit('ended', this.userID);
+  }
+
+  ngOnInit() {
+    this.getQueueSize();
+    
+    // Event Listeners
+    this.socket.on('username', (username) => {
+      this.username = username;
+    });
+
+    this.socket.on('chatMessagesAdmin', (msg, sender) => {
+      $('#chat-messages-admin').append($('<li>').text(msg + ' ~ ' + sender));
+      console.log('Admin recvd msg ' + msg + ' from sender ' + sender);
+    });
+
+    this.socket.on('userSocketid', (socketid) => {
+      this.socket.emit('ended', socketid);
+      this.getQueueSize();
+    });
+
+    this.socket.on('firstInLine', (username, socketid) => {
+      this.username = username;
+      this.userID = socketid;
+      this.haveUsername = true;
+    });
+
+    this.socket.on('updateQueue', (length) => {
+      this.queueSize = length;
+    });
+
+    this.socket.on('connectedWith', (user, id) => {
+      this.username = user;
+      this.userID = id;
+    });
+
+    // end chat
+    this.socket.on('ended', () => {
+      this.chatEnded = true;
+      this.socket.disconnect();
+      this.handleEndChat();
+    });
+  }
+
+}
